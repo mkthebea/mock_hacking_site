a7f8785a1fc892bb4ee17a9b6745bcf9997f5e5e
mkthebea <5596molly@naver.com>
2022-12-03 20:25:38 +0900

    filter user input

--- filtering.js
+++ filtering.js
@@ -1,16 +1,19 @@
 const checkSearchKeyword = () => {
   let keyword = document.getElementById("search_input").value;
+
   if (keyword.length > 0) {
     //특수문자 제거
-    var expText = /[%=><]/;
-    if (expText.test(keyword) == true) {
-      alert("특수문자를 입력 할수 없습니다.");
-      keyword.value = keyword.value.split(expText).join("");
+    let expText = /[%=*><]/;
+    if (expText.test(keyword)) {
+      alert("[No SQL Injection] 특수문자를 입력 할 수 없습니다.");
+      // console.log(keyword);
+      keyword = keyword.split(expText).join("");
+      // console.log(keyword);
       return false;
     }
 
     //특정문자열(sql예약어의 앞뒤공백포함) 제거
-    var sqlArray = new Array(
+    let sqlArray = new Array(
       //sql 예약어
       "OR",
       "SELECT",
@@ -26,13 +29,14 @@ const checkSearchKeyword = () => {
       "TRUNCATE"
     );
 
-    var regex;
-    for (var i = 0; i < sqlArray.length; i++) {
+    let regex;
+    for (let i = 0; i < sqlArray.length; i++) {
       regex = new RegExp(sqlArray[i], "gi");
-
       if (regex.test(keyword)) {
-        alert('"' + sqlArray[i] + '"와(과) 같은 특정문자로 검색할 수 없습니다.');
+        alert('[No SQL Injection] "' + sqlArray[i] + '"와(과) 같은 특정문자열로 검색할 수 없습니다.');
+        // console.log(keyword);
         keyword = keyword.replace(regex, "");
+        // console.log(keyword);
         return false;
       }
     }
